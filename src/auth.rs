dc94bbe1237bfdefc6b748316011f0e61985cf73
Mario Zupan <mario@zupzup.org>
2020-10-21 22:46:22 +0200

    rename uid

--- src/auth.rs
+++ src/auth.rs
@@ -49,14 +49,14 @@ pub fn with_auth(role: Role) -> impl Filter<Extract = (String,), Error = Rejecti
         .and_then(authorize)
 }
 
-pub fn create_jwt(user_id: &str, role: &Role) -> Result<String> {
+pub fn create_jwt(uid: &str, role: &Role) -> Result<String> {
     let expiration = Utc::now()
         .checked_add_signed(chrono::Duration::seconds(60))
         .expect("valid timestamp")
         .timestamp();
 
     let claims = Claims {
-        sub: user_id.to_owned(),
+        sub: uid.to_owned(),
         role: role.to_string(),
         exp: expiration as usize,
     };
